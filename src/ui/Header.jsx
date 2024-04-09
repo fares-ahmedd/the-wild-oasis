d774aad46298d6b7c0bbc4ff31ebae63d2d1bd27
fares <fares.haliim@gmail.com>
2024-04-09 20:19:54 +0200

    change header style

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -9,7 +9,7 @@ const StyledHeader = styled.header`
   display: flex;
   gap: 2.4rem;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
 `;
 function Header() {
   return (
