10885cb3ba4da468ee518ced140234bf56442e79
Fares Ahmed <fares.haliim@gmail.com>
2024-06-04 03:46:21 +0300

    hide supabase Api Key 🔐

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://wlxgverwozlrneawceqe.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndseGd2ZXJ3b3pscm5lYXdjZXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcyOTk2MjIsImV4cCI6MjAzMjg3NTYyMn0.zpet--oTfEVXVODnjUmglZufeAv8Q7UI8fYe_jzqe_k";
+const supabaseKey = import.meta.env.VITE_SUPABASE;
+console.log(supabaseKey);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
