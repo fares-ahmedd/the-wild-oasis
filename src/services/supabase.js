c9c7c83608c215fd567c4b8020aac737d8d3d595
Fares Ahmed <fares.haliim@gmail.com>
2024-06-04 04:56:24 +0300

    add changes

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://wlxgverwozlrneawceqe.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE;
-console.log(supabaseKey);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
