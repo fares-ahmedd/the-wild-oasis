d377e9777c97f920c81e2ad586ce21d28a467133
Fares Ahmed <fares.haliim@gmail.com>
2024-06-05 06:19:57 +0300

    fix login issue 🛠️⚙️

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,18 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
-      navigate("/dashboard");
+      queryClient.setQueriesData(["user"], user.user);
+      navigate("/dashboard", {
+        replace: true,
+      });
     },
     onError: () => {
-      toast.error("provided email or password incorrect");
+      toast.error("email or password incorrect");
     },
   });
 
